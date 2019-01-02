004104725836da677f1cdf2434ecfcecc3188ca3
richelbilderbeek <richel@richelbilderbeek.nl>
2019-01-02 11:12:22 +0100

    Can create epitopeome

--- R/create_epitopeome.R
+++ R/create_epitopeome.R
@@ -7,35 +7,38 @@ create_epitopeome <- function(
   alleles = "DRB1_0101",
   binding_strength_threshold = 0.05,
   tmhmm_filename = tempfile(fileext = "txt"),
-  netmhc2pan_filename = tempfile(fileext = ".csv")
+  netmhc2pan_filename = tempfile(fileext = ".csv"),
+  epitopeome_filename = tempfile(fileext = ".fasta")
 ) {
   testit::assert(file.exists(fasta_filename))
   testit::assert(tmhmm::is_tmhmm_installed())
   testit::assert(netmhc2pan::is_netmhc2pan_installed())
   testit::assert(all(alleles %in% netmhc2pan::get_netmhc2pan_alleles()))
   # TMHH
-  df_tmh <- tmhmm::run_tmhmm(fasta_filename)
-  utils::write.csv(
-    x = df_tmh,
-    file = tmhmm_filename,
-    row.names = FALSE
+  locatome <- tmhmm::run_tmhmm(fasta_filename)
+  writeLines(
+    text = locatome,
+    con = tmhmm_filename
   )
   # NetMHC2pan
   df_bindings <- netmhc2pan::run_netmhc2pan(
     fasta_filename = fasta_filename,
     alleles = alleles
   )
+  testit::assert(!is.character(df_bindings$Pos))
   df_strong_bindings <- df_bindings[
     df_bindings$Rank <= binding_strength_threshold,
   ]
   utils::write.csv(
-    x = df_bindings,
+    x = df_strong_bindings,
     file = netmhc2pan_filename,
     row.names = FALSE
   )
   return()
-  epitopeome::create_epitopeome_cpp(
+  create_epitopeome_file_cpp(
     tmhs_filename = tmhmm_filename,
-    netmhc2pan_filename = netmhc2pan_filename
+    netmhc2pan_filename = netmhc2pan_filename,
+    epitopeome_filename = epitopeome_filename
   )
+  readLines(epitopeome_filename)
 }
--- tests/testthat/test-create_epitopeome.R
+++ tests/testthat/test-create_epitopeome.R
@@ -10,13 +10,21 @@ test_that("use", {
   )
   tmhmm_filename <- tempfile(fileext = "txt")
   netmhc2pan_filename <- tempfile(fileext = ".csv")
+  epitopeome_filename <- tempfile(fileext = ".fasta")
+  binding_strength_threshold <- 100.0
+  alleles = "DRB1_0101"
 
   epitopeome <- create_epitopeome(
-    fasta_filename,
+    fasta_filename = fasta_filename,
+    alleles = alleles,
+    binding_strength_threshold = binding_strength_threshold,
     tmhmm_filename = tmhmm_filename,
-    netmhc2pan_filename = netmhc2pan_filename
+    netmhc2pan_filename = netmhc2pan_filename,
+    epitopeome_filename = epitopeome_filename
   )
 
   readLines(tmhmm_filename)
   readLines(netmhc2pan_filename)
+  skip("WIP")
+  readLines(epitopeome_filename)
 })
