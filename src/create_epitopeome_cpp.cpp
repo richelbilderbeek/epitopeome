873f0b3b79ed1342a79dc3255b95cb023bb233af
richelbilderbeek <richel@richelbilderbeek.nl>
2019-01-03 15:06:42 +0100

    No auto

--- src/create_epitopeome_cpp.cpp
+++ src/create_epitopeome_cpp.cpp
@@ -8,7 +8,9 @@
 #include <sstream>
 #include <iterator>
 #include <cstdlib>
+#include <cstdio>
 #include <vector>
+#include <cstring>
 
 using namespace std;
 
@@ -16,7 +18,7 @@ std::map<std::string, std::string> create_epitopeome_cpp(
   const std::string& tmhs_filename,
   const std::string& netmhc2pan_filename
 ) {
-  const auto locatome = create_locatome_cpp(tmhs_filename);
+  const std::map<std::string, std::string> locatome = create_locatome_cpp(tmhs_filename);
 
   std::map<std::string, std::string> m = locatome;
 
@@ -28,7 +30,7 @@ std::map<std::string, std::string> create_epitopeome_cpp(
     if (line == "\"Pos\",\"Peptide\",\"ID\",\"Allele\",\"one_minus_log50k\",\"nM\",\"Rank\"") continue;
     const std::vector<std::string> words = split_str(line, ',');
     assert(words.size() == 7);
-    const int pos = atoi(words[0].c_str());
+    const int pos = std::atoi(words[0].c_str());
     //const std::string peptide = words[1];
     const std::string quoted_id = words[2];
     const std::string id = quoted_id.substr(1, quoted_id.size() - 2);
